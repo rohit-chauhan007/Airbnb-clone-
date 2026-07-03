ff011db399c60a3d3908898493b0d9201f87dc52
rohit chauhan <rk9331498@gmail.com>
2026-07-04 00:18:30 +0530

    route change

--- app.js
+++ app.js
@@ -140,7 +140,7 @@ app.use((req,res,next)=>{
 //    res.send(registerUser);
 // });
 app.get("/", (req, res) => {
-  res.redirect("/listings");
+  res.redirect("/listing");
 });
 //routers 
 app.use("/listing",listing);
